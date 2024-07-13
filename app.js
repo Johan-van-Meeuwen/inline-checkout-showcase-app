85d4b2dc79c2cd50e8d2609d8d0cf7211399c20c
Johan van Meeuwen <j.meeuwen@MacBook-Pro.local>
2024-07-13 19:12:06 +0100

    Added index route

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.get('/env', (req, res) => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 app.get('/pricing', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/pricing.html'))
 })
